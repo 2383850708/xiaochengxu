9d9bfe5b749773fb5777297f5d63ec610f5c368d
wenyuankun <2383850708@qq.com>
2019-01-17 17:29:07 +0800

    add hot

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,5 @@
 Page({
   data: {
-    imgUrls: [
-      '../public/images/tooopen_sy_175833047715.jpg',
-      '../public/images/tooopen_sy_175833047715.jpg',
-      '../public/images/tooopen_sy_175833047715.jpg'
-    ],
-    indicatorDots: true,
-    vertical: false,
-    autoplay: true,
-    circular: true,
-    interval: 2000,
-    duration: 500,
-    previousMargin: 0,
-    nextMargin: 0,
-    navbarActiveIndex: 0,
     showtab: 0,  //顶部选项卡索引
     tabnav: {
       tabnum: 5,
