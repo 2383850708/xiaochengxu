f4e32cfbb25d5dc2ecdd91eec97194d990436c7a
wenyuankun <2383850708@qq.com>
2019-02-18 21:50:10 +0800

    2019-2-18

---
+++ pages/index/other-list-template/other_list.js
@@ -0,0 +1,28 @@
+var pageObject = {
+  data: {
+    
+  },
+  setDisabled: function (e) {
+    this.setData({
+      disabled: !this.data.disabled
+    })
+  },
+  setPlain: function (e) {
+    this.setData({
+      plain: !this.data.plain
+    })
+  },
+  setLoading: function (e) {
+    this.setData({
+      loading: !this.data.loading
+    })
+  },
+  onGotUserInfo: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
+}
+
+
+Page(pageObject)
